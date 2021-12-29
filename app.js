cc4e43b2b2bc0ae9ff5ac38227e8b550e69441e7
YANIV-LAPTOP\User <yaniv.leibovitz@gmail.com>
2021-12-29 13:35:59 +0200

    fix port for heroku use

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const geocode = require("./utils/geocode")
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.get('/', (req, res) => {
